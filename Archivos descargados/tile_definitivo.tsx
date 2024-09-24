3c233f8f5160cb0b7b880eed6353b4062ca4a74a
Kezior <fabianpinedaromero6@gmail.com>
2024-09-23 22:52:51 -0500

    Actualizacion
    Organizar las carpetas del proyecto (severo desastre)

--- Archivos descargados/tile_definitivo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tile_definitivo" tilewidth="16" tileheight="16" tilecount="1920" columns="48">
- <image source="../assets/images/tiles/tile_definitivo.png" width="768" height="640"/>
-</tileset>
